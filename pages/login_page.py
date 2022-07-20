1d1f9e0b636f59dcbc1ef5662182eaeb4cf0c3a8
GretskyO <37874780+GretskyO@users.noreply.github.com>
2022-07-21 02:04:07 +0600

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", "error"
+        assert "login" in self.browser.current_url, "Login link is not presented"
 
     def should_be_login_form(self):
         assert self.browser.find_element(By.XPATH, '//*[@id="login_form"]'), "Net formi logina"
